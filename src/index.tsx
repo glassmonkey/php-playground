f7de282ae586027d1eb01e0bb2cb1dbe5e7e66d8
glassmonkey <peace.infi0407@gmail.com>
2023-03-15 10:26:39 +0900

    feat: add contact method

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,54 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import { ChakraProvider, Heading, Center, Text, HStack } from "@chakra-ui/react";
-import App from "./app";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import {
+	ChakraProvider,
+	Heading,
+	Center,
+	Text,
+	HStack,
+	Flex,
+} from '@chakra-ui/react';
+import App from './app';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <App />
-  }
+	{
+		path: '*',
+		element: <App />,
+	},
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("app")!);
+const root = ReactDOM.createRoot(document.getElementById('app')!);
 root.render(
-  <ChakraProvider>
-    <Heading as="h1" style={{ margin: "16px" }}>
-      PHP Playground
-    </Heading>
-    <RouterProvider router={router} />
-    <Center>
-      <HStack spacing="16px">
-        <Text as="small" size="xs">
-          <a href="privacy.html" target="_blank">
-            Privacy Policy
-          </a>
-        </Text>
-        <Text as="small" size="xs">
-          &copy; 2023{" "}
-          <a href="https://twitter.com/glassmonekey" target="_blank">
-            glassmonekey
-          </a>
-        </Text>
-      </HStack>
-    </Center>
-  </ChakraProvider>
+	<ChakraProvider>
+		<Heading as="h1" style={{ margin: '16px' }}>
+			PHP Playground
+		</Heading>
+		<RouterProvider router={router} />
+		<Center>
+			<Flex direction="column" gap="8px">
+				<HStack spacing="16px">
+					<Text as="small" size="xs">
+						<a href="privacy.html" target="_blank">
+							Privacy Policy
+						</a>
+					</Text>
+					<Text as="small" size="xs">
+						<a
+							href="https://twitter.com/glassmonekey"
+							target="_blank"
+						>
+							Contact
+						</a>
+					</Text>
+				</HStack>
+				<Text as="small" size="xs">
+					&copy; 2023{' '}
+					<a href="https://twitter.com/glassmonekey" target="_blank">
+						@glassmonekey
+					</a>
+				</Text>
+			</Flex>
+		</Center>
+	</ChakraProvider>
 );
