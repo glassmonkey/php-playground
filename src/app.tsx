4139961aa9d34587f8a02c74bf33f000580237b9
glassmonkey <peace.infi0407@gmail.com>
2023-03-21 16:09:27 +0900

    rafactor: separate editor

--- src/app.tsx
+++ src/app.tsx
@@ -1,121 +1,12 @@
 import * as React from 'react';
-import { ReactElement, useEffect } from 'react';
-import { Spinner, Flex, Box, Spacer, Text } from '@chakra-ui/react';
-import { php as lnagPhp } from '@codemirror/lang-php';
-import { autocompletion, completionKeymap } from '@codemirror/autocomplete';
-import { useSandpack } from '@codesandbox/sandpack-react';
+import { useEffect } from 'react';
+import { Flex, Box, Spacer, Text } from '@chakra-ui/react';
 import { useSearchParams } from 'react-router-dom';
 import * as lzstring from 'lz-string';
 
-import {
-	SandpackProvider,
-	SandpackLayout,
-	SandpackCodeEditor,
-} from '@codesandbox/sandpack-react';
 import { Version, versions, asVersion } from './php-wasm/php';
-import { usePHP } from './php';
 import SelectPHP from './select';
-
-function PhpPreview(params: { version: Version }) {
-	const { sandpack } = useSandpack();
-	const { files, activeFile } = sandpack;
-	const code = files[activeFile].code;
-	const [loading, result] = usePHP(params.version, code);
-
-	if (loading) {
-		return <Spinner />;
-	}
-
-	return <iframe srcDoc={result} height="100%" width="100%" sandbox="" />;
-}
-
-function PhpCodeCallback(params: { onChangeCode: (code: string) => void }) {
-	const { sandpack } = useSandpack();
-	const { files, activeFile } = sandpack;
-	const code = files[activeFile].code;
-	params.onChangeCode(code);
-	return <></>;
-}
-
-function EditorLayout(params: { Editor: ReactElement; Preview: ReactElement }) {
-	return (
-		<Flex direction="column" padding="3" bg="gray.800" height="100%">
-			<Flex
-				justify="space-between"
-				direction={{ base: 'column', lg: 'row' }}
-				align="center"
-				gap="8px"
-				height="75vh"
-			>
-				<Box
-					as={SandpackLayout}
-					flexDirection={{ base: 'column', lg: 'row' }}
-					height={{ base: '50%', lg: '100%' }}
-					width="100%"
-				>
-					<Box
-						as="span"
-						flex="1"
-						height="100%"
-						maxWidth={{ base: '100%' }}
-						position="relative"
-						className="group"
-					>
-						{params.Editor}
-					</Box>
-				</Box>
-				<Box
-					width="100%"
-					height={{ base: '50%', lg: '100%' }}
-					style={{
-						backgroundColor: 'white',
-					}}
-				>
-					{params.Preview}
-				</Box>
-			</Flex>
-		</Flex>
-	);
-}
-
-function Editor(params: {
-	initCode: string;
-	version: Version;
-	onChangeCode: (code: string) => void;
-}) {
-	return (
-		<SandpackProvider
-			template="react"
-			files={{ '/app.php': params.initCode }}
-			options={{
-				activeFile: '/app.php', // used to be activePath
-				visibleFiles: ['/app.php'], // used to be openPaths
-			}}
-		>
-			<EditorLayout
-				Editor={
-					<SandpackCodeEditor
-						showRunButton={false}
-						showLineNumbers
-						showTabs={false}
-						style={{ height: '100%' }}
-						extensions={[autocompletion()]}
-						extensionsKeymap={[completionKeymap]}
-						additionalLanguages={[
-							{
-								name: 'php',
-								extensions: ['php'],
-								language: lnagPhp(),
-							},
-						]}
-					/>
-				}
-				Preview={<PhpPreview version={params.version} />}
-			/>
-			<PhpCodeCallback onChangeCode={params.onChangeCode} />
-		</SandpackProvider>
-	);
-}
+import { Editor } from "./editor";
 
 type UrlState = {
 	v: Version;
