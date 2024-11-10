5231a6adbd10988195492d5915ada0ac373561fb
永野峻輔 <peace.infi0407@gmail.com>
2024-11-11 01:35:22 +0900

    feat: add convert function

--- src/__test__/php.test.ts
+++ src/__test__/php.test.ts
@@ -1,5 +1,5 @@
 import { expect, it, describe } from 'vitest';
-import { initPHP, runPHP } from '../php';
+import {convertCodeToPhpPlayground, initPHP, runPHP} from '../php';
 // @ts-ignore
 import { mockFetch } from 'vi-fetch';
 // @ts-ignore
@@ -49,3 +49,22 @@ describe('show phpinfo()', async function () {
 		});
 	});
 });
+
+describe('convert code to php code for wasm', async function () {
+
+	const testCases = [
+		{"input": "<? echo(1);", "expected": "echo(1);"},
+		{"input": "<?php echo(1);", "expected": "echo(1);"},
+		{"input": "<?php\n echo(1);", "expected": "echo(1);"},
+		{"input": "<?\n echo(1);", "expected": "echo(1);"},
+		{"input": "Hello, World", "expected": "?>Hello, World"},
+		{"input": "<body><?echo 1+1?></body>", "expected": "?><body><?echo 1+1?></body>"},
+		{"input": "<? declare(strict_types=1); echo 1;", "expected": "declare(strict_types=1); echo 1;"},
+		{"input": "<body><? declare(ticks=1); echo 1+1?></body>", "expected": "?><body><? declare(ticks=1); echo 1+1?></body>"},
+	]
+
+	it.each(testCases)('input: %s to %s', async function (testCase) {
+		const actual = convertCodeToPhpPlayground(testCase.input);
+		expect(actual).toBe(testCase.expected);
+	})
+})
