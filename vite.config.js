0e4cfd41fd3abd631295613cd2fb6f1d865073ad
永野峻輔 <peace.infi0407@gmail.com>
2026-01-03 02:34:49 +0900

    chore: build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +16,17 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: 'src/workers',
+      filename: 'service-worker.ts',
+      injectManifest: {
+        injectionPoint: undefined
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module'
+      }
+    })
   ],
 })
