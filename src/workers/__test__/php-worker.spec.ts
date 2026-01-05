b35896b4ce38eb438cdb280b5b2d4fe567777b97
永野峻輔 <peace.infi0407@gmail.com>
2026-01-05 23:36:58 +0900

    fix: test

--- src/workers/__test__/php-worker.spec.ts
+++ src/workers/__test__/php-worker.spec.ts
@@ -7,37 +7,33 @@ import 'vi-fetch/setup';
 import { versions } from '../../php-wasm/php';
 
 describe('load wasm files', async function () {
-	versions.forEach(function (v) {
-		it(`version: ${v} should echo 1.`, async function () {
-			const data = fs.readFileSync(`assets/php-${v}.wasm`);
-			const pattern = `php-${v}\\.wasm.*`;
-			mockFetch('GET', new RegExp(pattern)).willResolve(data.buffer);
-			const actual = await runPHPInWorker(v, 'echo(1+1);');
-			expect(actual).toBe('2');
-		});
-	});
+	it.each(versions)('version: %s should load wasm file, eval(1+1=2)', async function (v) {
+		const data = fs.readFileSync(`assets/php-${v}.wasm`);
+		const pattern = `php-${v}\\.wasm.*`;
+		mockFetch('GET', new RegExp(pattern)).willResolve(data.buffer);
+		const actual = await runPHPInWorker(v, 'echo(1+1);');
+		expect(actual).toBe('2');
+	})
 });
 
 describe('show phpinfo()', async function () {
-	versions.forEach(function (v) {
-		it(`version: ${v} run phpinfo().`, async function () {
-			const data = fs.readFileSync(`assets/php-${v}.wasm`);
-			const pattern = `php-${v}\\.wasm.*`;
-			mockFetch('GET', new RegExp(pattern)).willResolve(data.buffer);
-			// Runtime error occurs, but you can ignore it because it is a problem with the way wasm is loaded.
-			let actual = await runPHPInWorker(v, 'phpinfo();');
-			// Shrink the request time
-			actual = actual.replace(
-				/(<tr>.+?REQUEST_TIME_FLOAT.+?<td.+?>)([\d.]+)(<\/td><\/tr>)/g,
-				'$1--$3'
-			);
-			actual = actual.replace(
-				/(<tr>.+?REQUEST_TIME.+?<td.+?>)([\d.]+)(<\/td><\/tr>)/g,
-				'$1--$3'
-			);
-			expect(actual).toMatchSnapshot();
-		});
-	});
+	it.each(versions)('version: %s run phpinfo().', async function (v) {
+		const data = fs.readFileSync(`assets/php-${v}.wasm`);
+		const pattern = `php-${v}\\.wasm.*`;
+		mockFetch('GET', new RegExp(pattern)).willResolve(data.buffer);
+		// Runtime error occurs, but you can ignore it because it is a problem with the way wasm is loaded.
+		let actual = await runPHPInWorker(v, 'phpinfo();');
+		// Shrink the request time
+		actual = actual.replace(
+			/(<tr>.+?REQUEST_TIME_FLOAT.+?<td.+?>)([\d.]+)(<\/td><\/tr>)/g,
+			'$1--$3'
+		);
+		actual = actual.replace(
+			/(<tr>.+?REQUEST_TIME.+?<td.+?>)([\d.]+)(<\/td><\/tr>)/g,
+			'$1--$3'
+		);
+		expect(actual).toMatchSnapshot();
+	})
 });
 
 describe('convert code to php code for wasm', async function () {
